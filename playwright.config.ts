cdb3af0b9a38901051c8f655b9dfbc87af0c4204
Roeland De Meester <105720+demeesterroel@users.noreply.github.com>
2026-07-16 14:25:42 +0200

    chore: add vercel protection bypass header to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,6 +60,9 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_PROTECTION_BYPASS_TOKEN ?? 'BYPASSORPLAYWRIGHTQWERTYUIOP123',
+    },
   },
 
   projects: [
